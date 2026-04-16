38b9030e90787ed034139dd47b5a9359dcb80665
JJB0619 <jiangbo0619@qq.com>
2026-04-16 10:35:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -13,21 +13,33 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
 
 class Warehouse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    products = db.relationship('Product', backref='warehouse', lazy=True)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     spec = db.Column(db.String(100), default='')
     stock = db.Column(db.Integer, default=0)
+    price = db.Column(db.Float, default=0.0)
+    note = db.Column(db.String(200), default='')
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
     warehouse_id = db.Column(db.Integer, db.ForeignKey('warehouse.id'), nullable=False)
     create_time = db.Column(db.DateTime, default=datetime.now)
+
+    @property
+    def total_price(self):
+        return round(self.stock * self.price, 2)
 
 class StockIn(db.Model):
     id = db.Column(db.Integer, primary_key=True)
